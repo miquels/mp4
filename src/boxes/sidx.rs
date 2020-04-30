c3dbea4adf01d4ff88cddaa461ff1d839177c47d
Miquel van Smoorenburg <mike@langeraar.net>
2020-05-01 01:51:15 +0200

    update

--- src/boxes/sidx.rs
+++ src/boxes/sidx.rs
@@ -7,24 +7,6 @@ use std::io;
 use crate::fromtobytes::{FromBytes, ToBytes, ReadBytes, WriteBytes};
 use crate::types::*;
 
-//  aligned(8) class TrackRunBox
-//  extends FullBox(‘trun’, version, tr_flags) {
-//      unsigned int(32) sample_count;
-//      // the following are optional fields
-//      signed int(32) data_offset;
-//      unsigned int(32) first_sample_flags;
-//      // all fields in the following array are optional
-//      {
-//          unsigned int(32) sample_duration;
-//          unsigned int(32) sample_size;
-//          unsigned int(32) sample_flags
-//          if (version == 0)
-//              { unsigned int(32) sample_composition_time_offset; }
-//          else
-//              { signed int(32) sample_composition_time_offset; }
-//      }[ sample_count ]
-//  }
-
 /// 8.16.3 Segment Index Box (ISO/IEC 14496-12:2015(E))
 #[derive(Debug)]
 pub struct SegmentIndexBox {
